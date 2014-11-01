dde9ac7b23b1ce1b293f92085a58af619f546b6f
Matthew Knudsen <mknudsen01@gmail.com>
2014-11-01 13:35:38 -0700

    Create main view with domready

--- client/app.js
+++ client/app.js
@@ -1,5 +1,14 @@
+var MainView = require('./views/main');
+var domready = require('domready');
+
 window.app = {
   init: function() {
+    var self = this;
+    domready( function() {
+      self.view = new MainView({
+        el: document.body
+      });
+    });
     console.log("Hello, world!");
   }
 };
---
+++ client/views/main.js
@@ -0,0 +1,6 @@
+var View = require('ampersand-view');
+
+module.exports = View.extend({
+  template: '<body>Hello, world</body>',
+  autoRender: true
+});
