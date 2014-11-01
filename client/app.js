1491b8f29c75920efb3fc0121089d03146e8f98b
Matthew Knudsen <mknudsen01@gmail.com>
2014-11-01 13:46:30 -0700

    install router

--- client/app.js
+++ client/app.js
@@ -1,15 +1,18 @@
 var MainView = require('./views/main');
 var domready = require('domready');
+var Router = require('./ampersand-router');
 
 window.app = {
   init: function() {
     var self = this;
     domready( function() {
+      self.router = new Router();
       self.view = new MainView({
         el: document.body
       });
     });
     console.log("Hello, world!");
+    self.router.history.start({pushState: true});
   }
 };
 
